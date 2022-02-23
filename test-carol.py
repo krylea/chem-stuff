df081c6b3ac627103bd9463cb0bc56ad8305ff25
Kira Selby <kaselby@uwaterloo.ca>
2022-02-23 11:19:35 -0500

    stuff

--- test-carol.py
+++ test-carol.py
@@ -17,6 +17,10 @@ import os
 
 import argparse
 
+def get_dirs(basedir):
+    subfolders = [f.path for f in os.scandir(basedir) if f.is_dir()]
+    return subfolders
+
 parser = argparse.ArgumentParser()
 parser.add_argument("run_folders", type=str, nargs='+')
 args = parser.parse_args()
@@ -47,43 +51,45 @@ def read_trajectory_extract_features(a2g, traj_path, xml=False):
     return data_objects
 
 idx=0
-for run_folder in args.run_folders:
-    assert os.path.exists(os.path.join(run_folder, "surface"))
-    subfolders = [ f.name for f in os.scandir(run_folder) if f.is_dir() if f.name != "surface"]
+for root_dir in args.rootdirs:
+    for facet_dir in get_dirs(root_dir):
+        for surface_dir in get_dirs(facet_dir):
+            assert os.path.exists(os.path.join(surface_dir, "surface"))
+            subfolders = [ f.name for f in os.scandir(surface_dir) if f.is_dir() if f.name != "surface"]
 
-    data_objects = read_trajectory_extract_features(a2g, os.path.join(run_folder, "surface", "OUTCAR"))
-    surface_energy = data_objects[1].y_relaxed
-    for runname in subfolders:
-            # Extract Data object
-        filename = os.path.join(run_folder, runname, "OUTCAR")
-        if os.path.exists(filename):
-            data_objects = read_trajectory_extract_features(a2g, filename)
-            initial_struc = data_objects[0]
-            relaxed_struc = data_objects[1]
-            
-            initial_struc.y_init = initial_struc.y - surface_energy # subtract off reference energy, if applicable
-            del initial_struc.y
-            initial_struc.y_relaxed = relaxed_struc.y - surface_energy # subtract off reference energy, if applicable
-            initial_struc.pos_relaxed = relaxed_struc.pos
-            
-            # Filter data if necessary
-            # OCP filters adsorption energies > |10| eV
-            if initial_struc.y_relaxed > 5:
-                print("energy too large")
-                continue
-            
-            initial_struc.sid = idx  # arbitrary unique identifier 
-            
-            # no neighbor edge case check
-            if initial_struc.edge_index.shape[1] == 0:
-                print("no neighbors", filename)
-                continue
-            
-            # Write to LMDB
-            txn = db.begin(write=True)
-            txn.put(f"{idx}".encode("ascii"), pickle.dumps(initial_struc, protocol=-1))
-            txn.commit()
-            db.sync()
-            idx += 1
+            data_objects = read_trajectory_extract_features(a2g, os.path.join(surface_dir, "surface", "OUTCAR"))
+            surface_energy = data_objects[1].y_relaxed
+            for runname in subfolders:
+                    # Extract Data object
+                filename = os.path.join(surface_dir, runname, "OUTCAR")
+                if os.path.exists(filename):
+                    data_objects = read_trajectory_extract_features(a2g, filename)
+                    initial_struc = data_objects[0]
+                    relaxed_struc = data_objects[1]
+                    
+                    initial_struc.y_init = initial_struc.y - surface_energy # subtract off reference energy, if applicable
+                    del initial_struc.y
+                    initial_struc.y_relaxed = relaxed_struc.y - surface_energy # subtract off reference energy, if applicable
+                    initial_struc.pos_relaxed = relaxed_struc.pos
+                    
+                    # Filter data if necessary
+                    # OCP filters adsorption energies > |10| eV
+                    if initial_struc.y_relaxed > 5:
+                        print("energy too large")
+                        continue
+                    
+                    initial_struc.sid = idx  # arbitrary unique identifier 
+                    
+                    # no neighbor edge case check
+                    if initial_struc.edge_index.shape[1] == 0:
+                        print("no neighbors", filename)
+                        continue
+                    
+                    # Write to LMDB
+                    txn = db.begin(write=True)
+                    txn.put(f"{idx}".encode("ascii"), pickle.dumps(initial_struc, protocol=-1))
+                    txn.commit()
+                    db.sync()
+                    idx += 1
 
 db.close()
