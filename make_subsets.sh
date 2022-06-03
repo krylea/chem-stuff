2f43a6ecc9a69655c484a4c2db5f2e360aa384a2
Kira Selby <kaselby@uwaterloo.ca>
2022-06-03 13:56:46 -0400

    make subsets

--- make_subsets.sh
+++ make_subsets.sh
@@ -9,7 +9,7 @@
 
 ele=$1
 
-python split_subset.py /scratch/hdd001/home/kaselby/ocp/element_data/1/$ele --prefix 50 --train_size 50 --val_size 1000
-python split_subset.py /scratch/hdd001/home/kaselby/ocp/element_data/1/$ele --prefix 200 --train_size 200 --val_size 1000
-python split_subset.py /scratch/hdd001/home/kaselby/ocp/element_data/1/$ele --prefix 1000 --train_size 1000 --val_size 1000
-python split_subset.py /scratch/hdd001/home/kaselby/ocp/element_data/1/$ele --prefix 5000 --train_size 5000 --val_size 1000
+python split_subset.py /scratch/hdd001/home/kaselby/ocp/element_data/1/$ele --train_size 50
+python split_subset.py /scratch/hdd001/home/kaselby/ocp/element_data/1/$ele --train_size 200
+python split_subset.py /scratch/hdd001/home/kaselby/ocp/element_data/1/$ele --train_size 1000
+python split_subset.py /scratch/hdd001/home/kaselby/ocp/element_data/1/$ele --train_size 5000
--- split-subset.py
+++ split-subset.py
@@ -9,19 +9,15 @@ parser = argparse.ArgumentParser()
 parser.add_argument("folder", type=str)
 parser.add_argument("--prefix", type=str)
 parser.add_argument("--train_size", type=int)
-parser.add_argument("--val_size", type=int)
 args = parser.parse_args()
 
-datapath = os.path.join(args.folder, "data.lmdb")
+datapath = os.path.join(args.folder, "train", "data.lmdb")
 dataset = SinglePointLmdbDataset({"src":datapath})
 N = len(dataset)
 N_train = args.train_size
-N_val, N_test = args.val_size, args.val_size
 
 indices = random.sample(list(range(N)), k=N)
 train_data = [dataset[i] for i in indices[:N_train]]
-val_data = [dataset[i] for i in indices[N_train:N_train+N_val]]
-test_data = [dataset[i] for i in indices[N_train+N_val:N_train+2*N_val]]
 
 def write_db(outdir, examples):
     if not os.path.exists(outdir):
@@ -42,6 +38,4 @@ def write_db(outdir, examples):
         idx += 1
     db.close()
 
-write_db(os.path.join(args.folder, args.prefix, "train"), train_data)
-write_db(os.path.join(args.folder, args.prefix, "val"), val_data)
-write_db(os.path.join(args.folder, args.prefix, "test"), test_data)
+write_db(os.path.join(args.folder, "train_%d" % args.train_size), train_data)
