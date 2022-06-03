e4f20ab9c17c7c8bd9c596bd2b67a405da579cb4
Kira Selby <kaselby@uwaterloo.ca>
2022-06-03 14:09:26 -0400

    make subsets

--- make_subsets.sh
+++ make_subsets.sh
@@ -10,7 +10,7 @@
 
 ele=$1
 
-python split_subset.py /scratch/hdd001/home/kaselby/ocp/element_data/1/$ele --train_size 50
-python split_subset.py /scratch/hdd001/home/kaselby/ocp/element_data/1/$ele --train_size 200
-python split_subset.py /scratch/hdd001/home/kaselby/ocp/element_data/1/$ele --train_size 1000
-python split_subset.py /scratch/hdd001/home/kaselby/ocp/element_data/1/$ele --train_size 5000
+python split-subset.py /scratch/hdd001/home/kaselby/ocp/element_data/1/$ele --train_size 50
+python split-subset.py /scratch/hdd001/home/kaselby/ocp/element_data/1/$ele --train_size 200
+python split-subset.py /scratch/hdd001/home/kaselby/ocp/element_data/1/$ele --train_size 1000
+python split-subset.py /scratch/hdd001/home/kaselby/ocp/element_data/1/$ele --train_size 5000
