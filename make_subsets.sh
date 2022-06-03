2f646ae8d94272bae96d54150f901a558110861b
Kira Selby <kaselby@uwaterloo.ca>
2022-06-03 13:50:35 -0400

    make subsets

--- make_subsets.sh
+++ make_subsets.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#SBATCH --job-name=test
+#SBATCH --output=logs/slurm-%j.txt
+#SBATCH --open-mode=append
+#SBATCH --ntasks=1
+#SBATCH --partition=cpu
+#SBATCH -c 2
+#SBATCH --mem=50GB
 
 ele=$1
 
